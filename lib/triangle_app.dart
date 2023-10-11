9bd93d1377a77f05b6c7bd6f8c11b30e5eca2a0b
Heri Sandiyadi <herisandiyadi@gmail.com>
2023-10-11 10:19:21 +0700

    recover

--- lib/triangle_app.dart
+++ lib/triangle_app.dart
@@ -21,7 +21,7 @@ String detectTriangle(num sideA, num sideB, num sideC) {
   }
 
   if (sides[0] + sides[1] <= sides[2]) {
-    throw Exception('ganti exception');
+    throw Exception('This is Inequal Tri-angle');
   }
 
   return "Segitiga Sembarang";
